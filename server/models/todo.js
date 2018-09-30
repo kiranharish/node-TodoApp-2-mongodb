7e7028fd4860d028e0cdaf82593ddab04a46e677
unknown <kiran>
2018-09-30 07:57:36 -0700

    todo post reqest setup

---
+++ server/models/todo.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+var Todo = mongoose.model('todo',{
+    text:{type:String},
+    completed:{type:Boolean},
+    completedAt:{type:Number}
+});
+
+module.exports = {Todo}
---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser=require('body-parser');
+
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require ('./models/todo');
+const {User} = require('./models/user')
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+    // console.log(req.body);
+
+    var todo1 = new Todo({
+        text:req.body.text,
+        completed:req.body.completed
+    })
+
+    todo1.save()
+    .then((doc)=>{
+        res.send(doc);
+    },(err)=>{
+        res.send(err);
+    })
+})
+
+
+app.listen(3000,()=>{
+    console.log('Server on port 3000!');
+})
